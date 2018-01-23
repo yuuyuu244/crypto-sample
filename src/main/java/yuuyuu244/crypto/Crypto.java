6e651ca4e2a252572d446000a276ecc0f914b9ca
yuki <aaa@ssss>
2018-01-23 13:40:14 +0900

    add Maven project

---
+++ src/main/java/yuuyuu244/crypto/Crypto.java
@@ -0,0 +1,19 @@
+package yuuyuu244.crypto;
+
+/**
+ * Hello world!
+ *
+ */
+public class Crypto {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void encrypto() {
+        
+    }
+    
+    public static void decrypto() {
+        
+    }
+}
